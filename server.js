5403b53f203f8a2e6b9e756d0184f67be96d9c3d
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2023-11-08 16:15:29 -0700

    addding new vcode. fixing mostly havent added

--- server.js
+++ server.js
@@ -1,29 +1,35 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const noteData = require('./db/db.json');
+const noteData = require('./Develop/db/db.json');
 const path = require('path');
 const PORT = 3000;
 
 
 
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + 'public'));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.get('*', (req, res) =>{
+app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
 
 
-app.get('/api/notes', (req, res) => res.json(noteData));
+app.get('/api/notes', (req, res) => {
+    res.status(200).json(noteData);
+});
 
 
 
-
+app.post('/api/notes', function (req, res, ) {
+    res.send(JSON.stringify(req.body));
+  });
 
 
 
